31012b2a4bca012b7a8363d896105853dd8224f5
Saurabh Kirve <saurabhkirve@gmail.com>
2021-07-05 00:37:22 +0530

    1.4

--- postioncalculaor2.2.py
+++ postioncalculaor2.2.py
@@ -7,16 +7,12 @@ print("Stop loss:")
 SL = input()
 print("Risk:")
 R = input()
-print("What you want to do?"+'1,2')
-T = input()
-if T =='1':
-   M = int(EP)-int(SL)
-   print('Qty:',int(R)/M)
-elif T == '2':
-     M = float(EP)-float(SL)
-     print('Qty:',float(R)/M)
+if EP is float and SL is float and R is int:
+   M = float(EP)-float(SL)
+   print('Qty:',float(R)/M)
+elif EP is int and SL is int and R is int:
+     M = int(EP)-int(SL)
+     print('Qty:',int(R)/M)
 else:
     print("Error")
 #this would be my first self tought project
-#i wanna thank me and my curious mind
-#this is a test
